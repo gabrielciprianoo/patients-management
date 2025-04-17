d849855ede0d3d3b44fc7a4d2e66e9586724096f
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-04-17 12:25:15 -0600

    feature: main layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import PatientsForm from "./components/PatientsForm";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black capitalize text-5xl text-center md:w-2/3 md:mx-auto">
+          seguimiento de pacientes {""}
+          <span className="text-indigo-700"> veterinaria</span>
+        </h1>
+
+        <div className="mt-12 md:flex">
+          <PatientsForm />
+          <div>
+            <h1>Patients List</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
