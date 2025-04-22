8980ea7319585cc618f20cc872d032538e18c64f
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-04-22 13:29:50 -0600

    refactor; use component item to show patient information

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import type { Patient } from "../types";
+import PatientItem from "./PatientItem";
 
 type PatientDetailsProps = {
   patient: Patient;
@@ -7,13 +8,28 @@ type PatientDetailsProps = {
 export default function PatientDetails({ patient }: PatientDetailsProps) {
     const {name, caretaker, date, email, symptoms } = patient;
   return (
-    <div>
-        <p>{name}</p>
-        <p>{caretaker}</p>
-        <p>{email}</p>
-        <p>{date.toString()}</p>
-        <p>{caretaker}</p>
-        <p>{symptoms}</p>
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+        <PatientItem label='nombre' value={name}/>
+        <PatientItem label='propietario' value={caretaker}/>
+        <PatientItem label='fecha de ingreso' value={date.toString()}/>
+        <PatientItem label='email' value={email}/>
+        <PatientItem label='sintomas' value={symptoms}/>
+
+        <div className="flex justify-between mt-10">
+          <button
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-800 uppercase py-2 px-10 cursor-pointer transition-all text-white font-bold rounded-md"
+          >
+            Editar
+          </button>
+
+          <button
+            type="button"
+            className="bg-red-600 hover:bg-red-800 uppercase py-2 px-10 cursor-pointer transition-all text-white font-bold rounded-md"
+          >
+            eliminar
+          </button>
+        </div>
     </div>
   );
 }
